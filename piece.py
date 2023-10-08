fc22e5f6d3ef92080db45d3e3d9f706c113bc5a7
Zanatticus <zingare@live.com>
2023-10-08 15:37:25 -0400

    updates

--- piece.py
+++ piece.py
@@ -1,25 +1,35 @@
 class Piece:
-    def __init__(self, coordinates, color)
-    
-    L = [[1,0],
-         [1,0],
-         [1,1]]
-    J = [[0,1],
-         [0,1],
-         [1,1]]
-    O = [[1,1],
-         [1,1]]
-    I = [[1],
-         [1],
-         [1],
-         [1]]
-    T = [[1,1,1],
-         [0,1,0]]
-    S = [[0,1,1],
-         [1,1,0]]
-    Z = [[1,1,0],
-         [0,1,1]]
-    
-    colors = {"O":"yellow", "I":"light_blue", "S":"red", "Z":"green", "L":"orange", "J":"pink", "T":"magenta"}
-    
-    
+     def __init__(self, piece_type):
+          s = self
+          #s.piece_list = ["I", "J", "L", "O", "S", "T", "Z"]
+          #s.piece_colors = {"I":"cyan", "J":"pink", "L":"orange", "O":"yellow", "S":"red", "T":"magenta", "Z":"green"}
+          if piece_type == "I":
+               s.color = "cyan"
+               s.coordinates = [[0,3], [0,4], [0,5], [0,6]]
+               
+          if piece_type == "J":
+               s.color = "pink"
+               s.coordinates = [[0,3], [1,3], [1,4], [1,5]]
+               
+          if piece_type == "L":
+               s.color = "orange"
+               s.coordinates = [[0,5], [1,3], [1,4], [1,5]]
+               
+          if piece_type == "O":
+               s.color = "yellow"
+               s.coordinates = [[0,4], [0,5], [1,4], [1,5]]
+               
+          if piece_type == "S":
+               s.color = "red"
+               s.coordinates = [[0,4], [0,5], [1,3], [1,4]]
+               
+          if piece_type == "T":
+               s.color = "magenta"
+               s.coordinates = [[0,4], [1,3], [1,4], [1,5]]
+               
+          if piece_type == "Z":
+               s.color = "green"
+               s.coordinates = [[0,3], [0,4], [1,4], [1,5]]
+               
+
+     
