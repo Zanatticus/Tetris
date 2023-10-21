cba9dd44f2df146cee0e79a90270f4af5d13a80d
Zanatticus <zingare@live.com>
2023-10-20 21:54:53 -0400

    prevented instant unhold

--- board.py
+++ board.py
@@ -93,21 +93,49 @@ class Board:
     
     # TODO when not empty it takes time to show respawn
     # TODO prevent reholding of same piece
+    # def hold_piece(s):
+    #     if s.holder != []:
+    #         if s.holder[0].held > 0:
+    #             return
+    #         held_piece = s.holder.pop(0)
+    #         s.current_piece.reset_piece()
+    #         s.current_piece.held = 1
+    #         for [row, col] in s.current_piece.coordinates:
+    #             s.board_array[row][col] = None
+    #         s.holder.append(s.current_piece)
+    #         s.current_piece = held_piece
+    #         s.spawn_piece
+    #     if s.holder == []:
+    #         for [row, col] in s.current_piece.coordinates:
+    #             s.board_array[row][col] = None
+    #         s.holder.append(s.current_piece)    
+    #         s.get_next_piece()  
+        
+    #     s.update_holder_array()
+    #     s.display_board()
+               
     def hold_piece(s):
-        for [row, col] in s.current_piece.coordinates:
-            s.board_array[row][col] = None
-            s.current_piece.reset_piece()
+
         if s.holder != []:
+            if s.holder[0].held > 0:
+                return
             held_piece = s.holder.pop(0)
             s.holder.append(s.current_piece)
+            for [row, col] in s.current_piece.coordinates:
+                s.board_array[row][col] = None
+                s.current_piece.reset_piece()
             s.current_piece = held_piece
             s.spawn_piece
         if s.holder == []:
+            for [row, col] in s.current_piece.coordinates:
+                s.board_array[row][col] = None
+                s.current_piece.reset_piece()
             s.holder.append(s.current_piece)    
-            s.get_next_piece()  
+            s.get_next_piece()
+        s.holder[0].held += 1
         s.update_holder_array()
-        s.display_board()
-               
+        s.display_board()           
+
     def update_holder_array(s):
         s.holder_array = [[None] * s.holder_columns for i in range(s.holder_rows)]
         for [row, col] in s.holder[0].queue_coordinates:
@@ -141,6 +169,8 @@ class Board:
                 return
         for [row, col] in s.current_piece.coordinates:
             s.board_array[row][col] = s.current_piece.piece_type
+        if s.holder != []:
+            s.holder[0].held -= 1
         s.display_board()
         
     def end_game(s):
--- piece.py
+++ piece.py
@@ -11,59 +11,73 @@ class Piece:
                s.queue_coordinates = [[0,0], [0,1], [0,2], [0,3]]
                s.pivot = 2 # 3rd coordinate [1, 5]
                s.position = 0
+               s.held = 0
           if piece_type == "J":
                s.color = "pink"
                s.coordinates = [[0,3], [1,3], [1,4], [1,5]]
                s.queue_coordinates = [[0,0], [1,0], [1,1], [1,2]]
                s.pivot = 2 # 3rd coordinate [1, 4]
                s.position = None
+               s.held = 0
           if piece_type == "L":
                s.color = "orange"
                s.coordinates = [[0,5], [1,3], [1,4], [1,5]]
                s.queue_coordinates = [[0,2], [1,0], [1,1], [1,2]]
                s.pivot = 2 # 3rd coordinate [1, 4]
                s.position = None
+               s.held = 0
           if piece_type == "O":
                s.color = "yellow"
                s.coordinates = [[0,4], [0,5], [1,4], [1,5]]
                s.queue_coordinates = [[0,1], [0,2], [1,1], [1,2]]
                s.pivot = None
                s.position = None
+               s.held = 0
           if piece_type == "S":
                s.color = "red"
                s.coordinates = [[0,4], [0,5], [1,3], [1,4]]
                s.queue_coordinates = [[0,1], [0,2], [1,0], [1,1]]
                s.pivot = 3 # 4th coordinate [1, 4]
                s.position = None
+               s.held = 0
           if piece_type == "T":
                s.color = "magenta"
                s.coordinates = [[0,4], [1,3], [1,4], [1,5]]
                s.queue_coordinates = [[0,1], [1,0], [1,1], [1,2]]
                s.pivot = 2 # 3rd coordinate [1, 4]
                s.position = None
+               s.held = 0
           if piece_type == "Z":
                s.color = "green"
                s.coordinates = [[0,3], [0,4], [1,4], [1,5]]
                s.queue_coordinates = [[0,0], [0,1], [1,1], [1,2]] 
                s.pivot = 2 # 3rd coordinate [1, 4]
                s.position = None
+               s.held = 0
 
      def reset_piece(s):
           if s.piece_type == "I":
                s.coordinates = [[1,3], [1,4], [1,5], [1,6]]
                s.position = 0
+               s.held = 1
           if s.piece_type == "J":
                s.coordinates = [[0,3], [1,3], [1,4], [1,5]]
+               s.held = 1
           if s.piece_type == "L":
                s.coordinates = [[0,5], [1,3], [1,4], [1,5]]
+               s.held = 1
           if s.piece_type == "O":
                s.coordinates = [[0,4], [0,5], [1,4], [1,5]]
+               s.held = 1
           if s.piece_type == "S":
                s.coordinates = [[0,4], [0,5], [1,3], [1,4]]
+               s.held = 1
           if s.piece_type == "T":
                s.coordinates = [[0,4], [1,3], [1,4], [1,5]]
+               s.held = 1
           if s.piece_type == "Z":
                s.coordinates = [[0,3], [0,4], [1,4], [1,5]]
+               s.held = 1
 
      def shift_piece(s, direction):
           if direction == "left":
